b974859956dbbdc42ee3668d57924777f2d43c65
EdinAyala <odayedin@gmail.com>
2025-05-12 19:36:58 -0600

    agregando configuracion

---
+++ conf.php
@@ -0,0 +1,37 @@
+<?php
+
+define('MODULO_DEFECTO', 'login');
+define('LAYOUT_LOGIN', 'login.php');
+define('LAYOUT_DESKTOP', 'desktop.php');
+define('MODULO_PATH', realpath('app/views'));
+define('LAYOUT_PATH', realpath('app/templates'));
+
+$conf['login'] = array(
+    'archivo'=>'login.html',
+    'layout'=>LAYOUT_LOGIN
+);
+
+$conf['inicio'] = array(
+    'archivo'=>'inicio.html',
+    'layout'=>LAYOUT_DESKTOP
+);
+
+$conf['peliculas'] = array(
+    'archivo'=>'peliculas.html',
+    'layout'=>LAYOUT_DESKTOP
+);
+
+$conf['tamales'] = array(
+    'archivo'=>'peliculas.html',
+    'layout'=>LAYOUT_DESKTOP
+);
+
+if(1==2){
+
+    $conf['pupusas'] = array(
+        'archivo'=>'peliculas.html',
+        'layout'=>LAYOUT_DESKTOP
+    );
+}
+
+?>
